e6e928dd9580906d7afa7439f02652c755006f3a
davidjhaa4u <davidjhaa4u@gmail.com>
2023-01-23 15:23:21 +0530

    folder created for organizing files

--- commands/organize.js
+++ commands/organize.js
@@ -10,5 +10,19 @@ let types = {
 }
 
 function organize(srcPath){
+    //1. to check if srcPath is present
+    if(srcPath == undefined){
+        //The process.cwd() method returns the current working directory of the Node.js process.
+        //console.log(srcPath); //undefined
+        srcPath = process.cwd();
+    }
+
+    let organized_Files = path.join(srcPath, "Organized Files");
+    if(fs.existsSync(organized_Files) == false){ //if organizedfiles nhi bana h to bana do warna rehne do
+        fs.mkdir(organized_Files);
+    }
+    else
+        console.log("Folder already exist");
+
     
 }
