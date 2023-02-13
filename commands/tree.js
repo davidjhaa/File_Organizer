d365c05ca5fc1263ae0f067615c98d1b4b4236de
davidjhaa4u <davidjhaa4u@gmail.com>
2023-02-13 13:13:00 +0530

    tree fn created

--- commands/tree.js
+++ commands/tree.js
@@ -1,7 +1,18 @@
-function tree(){
-    console.log("tree function");
+const fs = require("fs");
+
+const path = require("path");
+
+function tree(dirPath) {
+  if (dirPath == undefined) {
+    console.log("Please Enter a Valid Path");
+    return;
+  }
+  let doesExist = fs.existsSync(dirPath);
+  if (doesExist == true) {
+    treeHelper(dirPath, " ");
+  }
 }
 
 module.exports = {
-    tree : tree,
-}
+    tree: treeFn,
+  };
