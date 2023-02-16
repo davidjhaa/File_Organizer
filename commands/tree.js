83c0bba874238a9c99825fc4e6d40ab47992f0a8
davidjhaa4u <davidjhaa4u@gmail.com>
2023-02-16 14:02:57 +0530

    Code refactored

--- commands/tree.js
+++ commands/tree.js
@@ -11,8 +11,30 @@ function tree(dirPath) {
   if (doesExist == true) {
     treeHelper(dirPath, " ");
   }
+  else{
+    console.log("File Path Not Found");
+  }
+}
+
+function treeHelper(targetPath, indent) {
+  let isFile = fs.lstatSync(targetPath).isFile();
+
+  if (isFile == true) {
+      let fileName = path.basename(targetPath);
+      console.log(indent + "├── " + fileName);
+      return;
+  }
+  let dirName = path.basename(targetPath);
+  console.log(indent + "└──" + dirName);
+
+  let children = fs.readdirSync(targetPath);
+
+  for (let i = 0; i < children.length; i++) {
+    let childpath = path.join(targetPath, children[i]);
+    treeHelper(childpath, indent + "\t");
+  }
 }
 
 module.exports = {
-    tree: treeFn,
+    tree: tree,
   };
